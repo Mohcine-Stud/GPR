6d9a6b5e5ca41dda38b9a5427d3ff9c51fa23aa8
Mohcine-Stud <117465908+Mohcine-Stud@users.noreply.github.com>
2023-12-28 14:51:21 +0000

    Delete GestionPretRetour.Contracts directory

--- GestionPretRetour.Contracts/UserDemande/OrderBookRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿using GestionPretRetour.Contracts.Common;
-
-namespace GestionPretRetour.Contracts.UserDemande;
-
-public record OrderBookRequest(
-    string UserId,
-    List<Book> BooksList
-    );
